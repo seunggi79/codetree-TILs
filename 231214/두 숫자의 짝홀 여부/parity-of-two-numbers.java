60be4ec2232dd2c117e4031110c7477cd404effa
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2023-12-14 22:43:22 +0900

    Title: 두 숫자의 짝홀 여부, Time: 120ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231214/두 숫자의 짝홀 여부/parity-of-two-numbers.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String args[]) {
+        // 변수 선언 및 입력
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        
+        if (n % 2 == 0){
+            System.out.println("even");
+        }
+        if (m % 2 == 0){
+            System.out.println("odd");
+        }
+        
+    }
+}
