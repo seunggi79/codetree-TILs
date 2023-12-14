ed62b0aee6e3fb08022883b6707d6594a358f9bd
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2023-12-15 00:02:27 +0900

    Title: 점수 비교, Time: 151ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231215/점수 비교/score-comparison.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String args[]) {
+        // 변수 선언 및 입력
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+        
+
+        System.out.println(a>b && c > d?1:0);
+
+    }
+}
