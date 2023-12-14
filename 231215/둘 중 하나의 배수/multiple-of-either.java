b454c062115f1440e58534d3780047b31ad40991
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2023-12-15 00:26:47 +0900

    Title: 둘 중 하나의 배수, Time: 118ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231215/둘 중 하나의 배수/multiple-of-either.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String args[]) {
+        // 변수 선언 및 입력
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+
+        if (a % 3 ==0 || a % 5 == 0){
+            System.out.println(1);
+        }
+        else{
+            System.out.println(0);
+        }
+
+    }
+}
