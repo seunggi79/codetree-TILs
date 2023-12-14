5eb5650baec470dd48f904df62e151d7149c6aea
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2023-12-14 23:57:53 +0900

    Title: 4번의 크기 비교, Time: 142ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 231214/4번의 크기 비교/4-time-comparison.java
+++ 231214/4번의 크기 비교/4-time-comparison.java
@@ -10,9 +10,9 @@ public class Main {
         int d = sc.nextInt();
         int e = sc.nextInt();
 
-        System.out.println(a>=b?1:0);
+        System.out.println(a>b?1:0);
         System.out.println(a>c?1:0);
-        System.out.println(b>=d?1:0);
+        System.out.println(b>d?1:0);
         System.out.println(b>e?1:0);
 
     }
