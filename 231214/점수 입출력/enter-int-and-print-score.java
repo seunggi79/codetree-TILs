0f74994e29999f033069ea798b09b8c5d5f6888d
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2023-12-14 17:02:29 +0900

    Title: 점수 입출력, Time: 149ms, Memory: 10MB, Status: Passed - CodeTree

--- 231214/점수 입출력/enter-int-and-print-score.java
+++ 231214/점수 입출력/enter-int-and-print-score.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        System.out.println("Your score is "+a+" point");
+        System.out.println("Your score is "+a+" point.");
     }
 }
