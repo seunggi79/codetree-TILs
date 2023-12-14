74d91f01b4bb3e1bb2fae400335c5f3e45c3afff
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2023-12-14 22:32:01 +0900

    Title: 살 수 있는 물건 2, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231214/살 수 있는 물건 2/things-able-to-buy-2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String args[]) {
+        // 변수 선언 및 입력
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        if (n >= 3000){
+            System.out.println("book");
+        }
+        else if (n >= 1000){
+            System.out.println("mask");
+        }
+        else if (n >= 500){
+            System.out.println("pen");
+        }
+        else{
+            System.out.println("no");
+        }
+    }
+}
