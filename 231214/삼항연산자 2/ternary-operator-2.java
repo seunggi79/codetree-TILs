f9fb8c7a617fa6885ae7078db923c5606125ca20
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2023-12-14 22:00:31 +0900

    Title: 삼항연산자 2, Time: 116ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231214/삼항연산자 2/ternary-operator-2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main (String args[]) {
+        // 변수 선언 및 입력
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println( n == 1 ? "t" : "f");
+
+    }
+}
