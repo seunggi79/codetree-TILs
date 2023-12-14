162deb9b3143ababdd688352aa5666dfe8a15245
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2023-12-15 00:51:13 +0900

    Title: 굉장한 숫자, Time: 136ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231215/굉장한 숫자/amazing-number.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String args[]) {
+        // 변수 선언 및 입력
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+
+        if (a % 3 ==0 && a%2 !=0 || a%2 !=0 &&a % 5 == 0){
+            System.out.println("True");
+        }
+        else{
+            System.out.println("False");
+        }
+
+    }
+}
