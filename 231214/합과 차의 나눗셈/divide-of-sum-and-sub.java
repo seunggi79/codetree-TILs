4625c1ec102fc340bcc43f574fa7fa20fcd3358e
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2023-12-14 18:22:30 +0900

    Title: 합과 차의 나눗셈, Time: 136ms, Memory: 11MB, Status: Wrong Answer - CodeTree

---
+++ 231214/합과 차의 나눗셈/divide-of-sum-and-sub.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String args[]) {
+        // 변수 선언 및 입력
+        Scanner sc = new Scanner(System.in);
+        double n = sc.nextDouble();
+        double a = sc.nextDouble();
+
+
+
+        System.out.print((n+a)/(n-a));
+
+
+    }
+}
