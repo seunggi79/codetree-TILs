7ffc57acd709fc2f79eb5484dfb1ca15b2b325ff
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2023-12-15 00:12:06 +0900

    Title: 시력 검사, Time: 134ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 231215/시력 검사/eye-test.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String args[]) {
+        // 변수 선언 및 입력
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+
+        if (a >=1.0 && b>=1.0){
+            System.out.println("High");
+        }
+        else if (a >= 0.5 && b >= 0.5){
+            System.out.println("Middle");
+        }
+        else{
+            System.out.println("Low");
+        }
+
+    }
+}
