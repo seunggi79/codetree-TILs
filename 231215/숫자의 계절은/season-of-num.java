939b80cecd06d498d902276af4ca3fe8432dfc0f
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2023-12-15 00:58:09 +0900

    Title: 숫자의 계절은, Time: 122ms, Memory: 10MB, Status: Passed - CodeTree

--- 231215/숫자의 계절은/season-of-num.java
+++ 231215/숫자의 계절은/season-of-num.java
@@ -7,12 +7,12 @@ public class Main {
         int a = sc.nextInt();
 
 
-        if (a >=3 || a<=5){
+        if (a >=3 && a<=5){
             System.out.println("Spring");
-        } else if (a >= 6 || a<=8) {
+        } else if (a >= 6 && a<=8) {
             System.out.println("Summer");
             
-        } else if (a >= 9 || a<= 11) {
+        } else if (a >= 9 && a<= 11) {
             System.out.println("Fall");
         }
         else{
