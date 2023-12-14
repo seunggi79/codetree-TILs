45d1f16163dc83411a59613f4f52741e7810d906
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2023-12-14 17:43:38 +0900

    Title: 입력받아 출력 2, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231214/입력받아 출력 2/input-and-output-2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String args[]) {
+        // 변수 선언 및 입력
+        Scanner sc = new Scanner(System.in);
+        sc.useDelimiter("-");
+        int h = sc.nextInt();
+        int m = sc.nextInt();
+
+        // 출력
+        System.out.println(h+""+m);
+    }
+}
