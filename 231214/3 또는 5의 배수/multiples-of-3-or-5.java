fe170b13f21c4d746cf67efaf3ded868cf1405be
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2023-12-14 23:00:02 +0900

    Title: 3 또는 5의 배수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 231214/3 또는 5의 배수/multiples-of-3-or-5.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String args[]) {
+        // 변수 선언 및 입력
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        if (n % 3 == 0){
+            System.out.println("YES");
+        }
+        else{
+            System.out.println("NO");
+        }
+
+        if (n % 5 == 0){
+            System.out.println("YES");
+        }
+        else{
+            System.out.println("NO");
+        }
+        
+    }
+}
