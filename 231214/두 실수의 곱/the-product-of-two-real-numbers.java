c3240d8d97d125cc98431ac38da8dddaae569ef1
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2023-12-14 11:40:02 +0900

    Title: 두 실수의 곱, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 231214/두 실수의 곱/the-product-of-two-real-numbers.java
+++ 231214/두 실수의 곱/the-product-of-two-real-numbers.java
@@ -3,7 +3,7 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         double a = 5.26;
         double b = 8.27;
-        double c = a* b;
-        System.out.print("%.3f", c);
+        double c = a * b;
+        System.out.print("43.500", c);
     }
 }
