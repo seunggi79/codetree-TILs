494500f26be31a3ef454cbebcd1a246ee978f860
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2023-12-15 00:10:16 +0900

    Title: 범위 안의 수, Time: 122ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231215/범위 안의 수/number-in-range.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String args[]) {
+        // 변수 선언 및 입력
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        
+        if (a>=10 && a<=20){
+            System.out.println("yes");
+        }
+        else{
+            System.out.println("no");
+        }
+        
+    }
+}
