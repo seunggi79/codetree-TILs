04e8b5b2a19895a3f04248069f1b2d21c394e464
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2023-12-15 18:04:44 +0900

    Title: 증가시키며 출력하기 2, Time: 100ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 231215/증가시키며 출력하기 2/increase-and-print-2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String args[]) {
+
+        // 변수 선언 및 입력
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//        int m = sc.nextInt();
+        int i = 10;
+        
+        while (i <= 26){
+            System.out.println(i);
+            i++;
+        }
+
+
+
+    }
+}
