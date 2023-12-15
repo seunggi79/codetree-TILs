bad7883ef3ca812fd79610d06649d58a1562fedb
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2023-12-15 17:34:33 +0900

    Title: b부터 a까지 감소, Time: 140ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231215/b부터 a까지 감소/dec-from-b-to-a.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String args[]) {
+
+        // 변수 선언 및 입력
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        for (int i =m; i>=n; i--){
+//            if(i % 2 != 0){
+//            }
+            System.out.print(i+" ");
+
+        }
+
+
+
+    }
+}
