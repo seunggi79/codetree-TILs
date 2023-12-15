8b385bccd2ee58cff5ed659794c8f2292abcd6dc
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2023-12-15 21:29:55 +0900

    Title: 짝수만 출력 2, Time: 3072ms, Memory: 25MB, Status: Time Limit Exceed - CodeTree

---
+++ 231215/짝수만 출력 2/output-only-even-2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String args[]) {
+
+        // 변수 선언 및 입력
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int i = 1;
+        while (n >= i){
+            if (n % 2 == 0){
+                System.out.print(n + " ");
+
+            }
+
+            i-=2;
+        }
+        
+
+
+    }
+}
