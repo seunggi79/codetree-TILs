f63e1fd0f0b9b4b569788dbcc9a5a613dcc9e39d
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2023-12-15 17:16:12 +0900

    Title: 차례로 출력, Time: 83ms, Memory: 8MB, Status: Passed - CodeTree

--- 231215/차례로 출력/print-in-order.java
+++ 231215/차례로 출력/print-in-order.java
@@ -1,10 +1,8 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main (String args[]) {
 
         for(int i =5; i<= 17; i++){
-            System.out.println(i);
+            System.out.print(i+" ");
         }
     }
 }
