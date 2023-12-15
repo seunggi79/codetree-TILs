a6a8eeec888af04a1b0c53e9a37e303e9d75d9f0
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2023-12-15 16:04:27 +0900

    Title: 두 사람, Time: 165ms, Memory: 10MB, Status: Passed - CodeTree

--- 231215/두 사람/two-person.java
+++ 231215/두 사람/two-person.java
@@ -1,26 +1,16 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main (String args[]) {
-        // 변수 선언 및 입력
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        char a2 = sc.next().charAt(0);
-        int b = sc.nextInt();
-        char b2 = sc.next().charAt(0);
-
-
-        if ((a>=19 || b>= 19) && (a2 == 'M' || b2 == 'M')){
-            if( a2 == 'M' || b2 == 'M' ){
-                System.out.println(1);
-            }
-            else{
-                System.out.println(0);
-            }
-        }
-        else{
+        int aAge = sc.nextInt();
+        String aSex = sc.next();
+        int bAge = sc.nextInt();
+        String bSex = sc.next();
+        if ((aAge >= 19 && aSex.equals("M")) || (bAge >= 19 && bSex.equals("M"))) {
+            System.out.println(1);
+        } else {
             System.out.println(0);
         }
-
     }
 }
