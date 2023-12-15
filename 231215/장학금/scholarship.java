8c0c0053f0a28131ca03e8ac60c3388c9ae2b1b7
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2023-12-15 15:07:11 +0900

    Title: 장학금, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231215/장학금/scholarship.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String args[]) {
+        // 변수 선언 및 입력
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+
+        if ( a>=90 && b >= 95){
+            System.out.println(100000);
+        } else if (a>=90 && b >= 90) {
+            System.out.println(50000);
+
+        } 
+        else{
+            System.out.println(0);
+        }
+
+
+    }
+}
