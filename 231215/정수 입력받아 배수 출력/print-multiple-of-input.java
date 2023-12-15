aef28321f42b00ad98c60d03914cb688a0951137
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2023-12-15 17:30:22 +0900

    Title: 정수 입력받아 배수 출력, Time: 117ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231215/정수 입력받아 배수 출력/print-multiple-of-input.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String args[]) {
+
+        // 변수 선언 및 입력
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i =n; i<=n*5; i+=2){
+            System.out.println(i+" ");
+        }
+
+
+
+    }
+}
