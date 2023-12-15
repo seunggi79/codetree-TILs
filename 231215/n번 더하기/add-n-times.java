2a7d7e91ec5f8d6e77dbdccafb68a17cf15bd806
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2023-12-15 22:05:30 +0900

    Title: n번 더하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 231215/n번 더하기/add-n-times.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String args[]) {
+
+        // 변수 선언 및 입력
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int n = k;
+        for(int i =0; i<k; i++){
+            System.out.println(n+m);
+            n += m;
+        }
+
+
+    }
+}
