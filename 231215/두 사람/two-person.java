663b04867891ded36c93f3fc6e211319320435c2
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2023-12-15 15:21:28 +0900

    Title: 두 사람, Time: 113ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 231215/두 사람/two-person.java
+++ 231215/두 사람/two-person.java
@@ -5,26 +5,22 @@ public class Main {
         // 변수 선언 및 입력
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        char a2 = sc.next().charAt(0);
+        String a2 = sc.next();
         int b = sc.nextInt();
-        char b2 = sc.next().charAt(0);
+        String b2 = sc.next();
 
 
         if (a>=19 || b>= 19){
-            if( a2 == 'M' || b2 =='M' ){
+            if( a2.equals('M') || b2.equals('M') ){
                 System.out.println(1);
             }
             else{
                 System.out.println(0);
             }
-
         }
         else{
             System.out.println(0);
         }
-
-
-
 
     }
 }
