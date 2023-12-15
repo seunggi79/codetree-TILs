90304bca054524d49aa50eca7891a9667a68a564
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2023-12-15 18:06:49 +0900

    Title: n까지 숫자 출력, Time: 118ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231215/n까지 숫자 출력/print-1-to-n.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String args[]) {
+
+        // 변수 선언 및 입력
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+//        int m = sc.nextInt();
+        int i = 1;
+        while (i <= n){
+            System.out.print(n+" ");
+            i++;
+        }
+
+
+
+    }
+}
