bc4158eecb24668607104efa33bb68476fc00ae0
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2023-12-15 18:28:20 +0900

    Title: 감소시키며 출력하기 2, Time: 91ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231215/감소시키며 출력하기 2/decrease-and-print-2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String args[]) {
+
+        // 변수 선언 및 입력
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//        int m = sc.nextInt();
+        int i = 26;
+        while (i >= 10){
+//            if (i % 2 == 0){
+
+//            }
+            System.out.print(i + " ");
+
+            i-=1;
+        }
+
+
+
+    }
+}
