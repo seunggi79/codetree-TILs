936ce79f876ead16bb6c67b52759c916c5bf0dd6
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2023-12-15 21:33:36 +0900

    Title: 문자 출력하기, Time: 126ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 231215/문자 출력하기/print-text.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String args[]) {
+
+        // 변수 선언 및 입력
+        Scanner sc = new Scanner(System.in);
+        char n = sc.next().charAt(0);
+        for(int i = 0; i<8; i++){
+            System.out.println(n);
+        }
+        
+
+
+
+    }
+}
