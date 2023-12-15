b6a076e6941473daf4c4d0e6a2035a1633b3bef9
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2023-12-15 16:25:08 +0900

    Title: 윤년인가, Time: 151ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231215/윤년인가/is-leap-year.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String args[]) {
+
+        // 변수 선언 및 입력
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+        if ((a % 4 ==0 && a % 100 ==0 && a % 400 == 0)||(a % 4 == 0)){
+            System.out.println("True");
+        }
+        else{
+            System.out.println("False");
+        }
+    }
+}
