751b4b2f711fee4021c1ab43ee15290be6c8cbd8
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2023-12-17 22:10:04 +0900

    Title: 100부터 입력받는 수까지의 합, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231217/100부터 입력받는 수까지의 합/sum-of-the-number-input-from-100.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String args[]) {
+
+        // 변수 선언 및 입력
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+        int n = sc.nextInt();
+
+        for (int i = n; i <= 100; i++){
+            sum += i;
+        }
+        System.out.println(sum);
+
+
+    }
+
+}
