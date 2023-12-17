515bbedec057334fe78f680f4d4faf371fbd9556
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2023-12-18 01:14:16 +0900

    Title: 1부터 곱셈하기, Time: 132ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 231218/1부터 곱셈하기/multiple-from-one.java
+++ 231218/1부터 곱셈하기/multiple-from-one.java
@@ -10,7 +10,7 @@ public class Main {
 //        int m = sc.nextInt();
         for (int i = 1;i<=10;i++){
             sum *= i;
-            if (i >= sum){
+            if (i >= sum&& sum != 1){
                 sum = i;
                 break;
             }
