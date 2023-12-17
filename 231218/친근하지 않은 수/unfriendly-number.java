31937a97e3919afb5621856d865569530764704a
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2023-12-18 00:52:03 +0900

    Title: 친근하지 않은 수, Time: 127ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231218/친근하지 않은 수/unfriendly-number.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+
+        // 변수 선언 및 입력
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+//        int m = sc.nextInt();
+        int cnt = 0;
+
+        for (int i = 1; i<=n; i++){
+            if (i % 2 == 0|| i % 3 ==0|| i % 5 ==0 ){
+                cnt += 1;
+            }
+        }
+
+        System.out.print(cnt);
+
+    }
+}
