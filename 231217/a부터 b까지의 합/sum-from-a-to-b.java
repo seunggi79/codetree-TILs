18e97056962c2286973a1d7d8cd3f2e3a0eaf7b6
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2023-12-17 22:09:16 +0900

    Title: a부터 b까지의 합, Time: 154ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231217/a부터 b까지의 합/sum-from-a-to-b.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String args[]) {
+
+        // 변수 선언 및 입력
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        for (int i = n; i<= m; i++){
+            sum += i;
+        }
+        System.out.println(sum);
+
+
+    }
+
+}
