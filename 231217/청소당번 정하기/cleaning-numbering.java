6bdf28591ce4b6c59a91eed3b656b51fc78e0005
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2023-12-17 21:29:42 +0900

    Title: 청소당번 정하기, Time: 142ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231217/청소당번 정하기/cleaning-numbering.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String args[]) {
+
+        // 변수 선언 및 입력
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int cnt3 = 0;
+        int cnt5 = 0;
+        int cnt7 = 0;
+        for (int i = 1; i <=n; i++){
+
+            if(i % 2 == 0){
+                cnt3 += 1;
+            }
+            if(n % 3 ==0 ){
+                cnt5 += 1;
+            }
+            if(n% 12 == 0){
+                cnt7 += 1;
+            }
+
+        }
+        System.out.print(cnt3+" ");
+        System.out.print(cnt5+" ");
+        System.out.print(cnt7);
+
+    }
+
+}
