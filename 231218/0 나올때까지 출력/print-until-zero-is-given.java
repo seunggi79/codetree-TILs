ef4abbcaf79829d4844837577439f59b33bce920
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2023-12-18 01:24:22 +0900

    Title: 0 나올때까지 출력, Time: 159ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231218/0 나올때까지 출력/print-until-zero-is-given.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+
+        // 변수 선언 및 입력
+        Scanner sc = new Scanner(System.in);
+        int cnt = 1;
+//        
+        while(true){
+            int n = sc.nextInt();
+            System.out.println(n);
+            if(n == 0){
+
+                break;
+            }
+        }
+
+    }
+}
