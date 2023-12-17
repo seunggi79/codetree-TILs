cc952d7f243c0971a23d150ca93e798d877ba877
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2023-12-18 00:44:38 +0900

    Title: 주어진 수까지의 곱, Time: 150ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231218/주어진 수까지의 곱/multiplication-up-to-a-given-number.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+
+        // 변수 선언 및 입력
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int prod =1;
+
+        for (int i = n; i<=m; i++){
+       
+            prod *= i;
+
+
+        }
+
+        System.out.print(prod);
+
+    }
+}
