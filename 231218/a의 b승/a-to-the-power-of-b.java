8d3535fa0cc580b98048f9db558abcb28e205be8
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2023-12-18 00:46:43 +0900

    Title: a의 b승, Time: 121ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231218/a의 b승/a-to-the-power-of-b.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+
+        // 변수 선언 및 입력
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int prod =1;
+        int cnt =0;
+        while(cnt <= m){
+            
+            prod *= n;
+            cnt++;
+
+        }
+
+        System.out.print(prod);
+
+    }
+}
