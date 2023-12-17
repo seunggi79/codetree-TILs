476822d818500ef8c499758d1a2134e1a3ee1bdb
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2023-12-17 19:26:43 +0900

    Title: a 부터 b 까지, Time: 122ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231217/a 부터 b 까지/a-to-b.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String args[]) {
+
+        // 변수 선언 및 입력
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        for (int i = n; i<=m; i++){
+            System.out.print(i + " ");
+            if (i % 2 != 0){
+                i *= 2;
+            }else {
+                i += 3;
+            }
+            
+            
+
+        }
+
+
+    }
+}
