16865a480121be495c48ecd18af48d97fd8e61c7
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2023-12-18 01:27:46 +0900

    Title: 출석 부르기 2, Time: 121ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231218/출석 부르기 2/calling-attendance-2.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+
+        // 변수 선언 및 입력
+        Scanner sc = new Scanner(System.in);
+        int cnt = 1;
+//
+        while(true){
+            int n = sc.nextInt();
+
+
+            if(n == 1){
+                System.out.println("John");
+                break;
+            } else if (n == 2) {
+                System.out.println("Tom");
+            }
+            else if(n == 3){
+                System.out.println("Paul");
+            } else if (n == 4) {
+                System.out.println("Sam");
+                
+            }
+            else{
+                System.out.println("Vacancy");
+            }
+        }
+
+    }
+}
