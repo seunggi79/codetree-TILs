86003086a9eab795b914b6d263285e5a317445da
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2023-12-18 01:39:05 +0900

    Title: 20대의 평균나이, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 231218/20대의 평균나이/average-age-of-20.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+
+        // 변수 선언 및 입력
+        Scanner sc = new Scanner(System.in);
+        int cnt = 0;
+//      
+        int sum = 0;
+        while(true){
+            int n = sc.nextInt();
+            sum += n;
+            cnt += 1;
+            if (n > 29 && n <20 ){
+                
+                break;
+            }
+        }
+        System.out.printf("%.1f",(float)sum/cnt);
+    }
+}
