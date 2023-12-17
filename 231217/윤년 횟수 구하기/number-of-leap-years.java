ae4de0e99545d25693f580ed27b5a818d90b80a0
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2023-12-17 22:00:55 +0900

    Title: 윤년 횟수 구하기, Time: 161ms, Memory: 10MB, Status: Passed - CodeTree

--- 231217/윤년 횟수 구하기/number-of-leap-years.java
+++ 231217/윤년 횟수 구하기/number-of-leap-years.java
@@ -9,13 +9,14 @@ public class Main {
         int cnt = 0;
 
         for (int i =1; i<=n; i++){
-            if (i % 4 == 0){
-                cnt += 1;
-            }
-            else if (i % 4 == 0 && i % 100 == 0){
+            
+            if (i % 4 == 0 && i % 100 == 0){
                 if(i % 400== 0){
                     cnt+=1;
                 }
+            }
+            else if (i % 4 == 0){
+                cnt += 1;
             }
 
 
