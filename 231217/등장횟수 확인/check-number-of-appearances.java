863f9a8899dda69f7cb06b08762428d10ad4a3a5
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2023-12-17 22:07:38 +0900

    Title: 등장횟수 확인, Time: 122ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231217/등장횟수 확인/check-number-of-appearances.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String args[]) {
+
+        // 변수 선언 및 입력
+        Scanner sc = new Scanner(System.in);
+        int cnt = 0;
+        
+        for (int i = 0; i< 5; i++){
+            int n = sc.nextInt();
+            if(i % 2 == 0){
+                cnt += 1;
+            }
+        }
+        System.out.println(cnt);
+        
+        
+    }
+
+}
