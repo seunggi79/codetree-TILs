9eb981d75665a06554606e2078a29bac3bd7fc3f
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2023-12-18 01:55:06 +0900

    Title: 조건에 따른 연산, Time: 150ms, Memory: 10MB, Status: Passed - CodeTree

--- 231218/조건에 따른 연산/operatino-by-rule.java
+++ 231218/조건에 따른 연산/operatino-by-rule.java
@@ -5,7 +5,7 @@ public class Main {
 
         // 변수 선언 및 입력
         Scanner sc = new Scanner(System.in);
-        int cnt = 1;
+        int cnt = 0;
 
         int n = sc.nextInt();
         while(true){
