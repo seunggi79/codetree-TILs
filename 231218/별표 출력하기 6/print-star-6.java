1a8f5f4368b37ee1c6e3f9bd866c4e7ae40c0e4c
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2023-12-18 23:30:07 +0900

    Title: 별표 출력하기 6, Time: 139ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231218/별표 출력하기 6/print-star-6.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+
+        // 변수 선언 및 입력
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+
+        for(int i = 0; i < n; i++){
+
+            for(int j = 0; j<i; j++){
+                System.out.print("  ");
+            }
+
+            for(int k = 0; k < (2*n)-(2*i)-1; k++){
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+        for(int i = 0; i < n-1; i++){
+
+            for(int j = 0; j<n-i-2; j++){
+                System.out.print("  ");
+            }
+
+            for(int k = 0; k < 3+(2*i); k++){
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+
+
+    }
+}
