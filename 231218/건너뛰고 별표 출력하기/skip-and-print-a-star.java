2c4dcfefc894f3238656f9813301283d7df32601
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2023-12-18 20:58:49 +0900

    Title: 건너뛰고 별표 출력하기, Time: 139ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231218/건너뛰고 별표 출력하기/skip-and-print-a-star.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+
+        // 변수 선언 및 입력
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+            System.out.println();
+        }
+
+        for(int i = 0; i<n-1;i++){
+            for(int j = n-1; j > i ; j--){
+                System.out.print("*");
+            }
+            System.out.println();
+            System.out.println();
+        }
+    }
+}
