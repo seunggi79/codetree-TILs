60989d74749e014530c64c1a95f0a0128b328f75
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2023-12-18 14:12:40 +0900

    Title: 소수 판별기, Time: 126ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231218/소수 판별기/prime-number-discriminator.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+
+        // 변수 선언 및 입력
+        Scanner sc = new Scanner(System.in);
+        boolean satisfied= false;
+
+        int n = sc.nextInt();
+       
+
+        for (int i = 1; i<=n; i++){
+            if(n % i == 0){
+                satisfied = true;
+            }
+        }
+        if(satisfied){
+            System.out.println('P');
+        }
+        else {
+            System.out.println('C');
+        }
+
+    }
+}
