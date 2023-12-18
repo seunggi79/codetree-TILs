277bd1eeaac99b0b67e4653a63b23cfa7e1b7d61
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2023-12-18 14:49:52 +0900

    Title: 별표 출력하기 2, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

--- 231218/별표 출력하기 2/print-star-2.java
+++ 231218/별표 출력하기 2/print-star-2.java
@@ -10,7 +10,7 @@ public class Main {
 
         for (int i =0; i<n; i++){
             for (int j = 0; j<n-i; j++){
-                System.out.print("*");
+                System.out.print("* ");
 
             }
             System.out.println();
