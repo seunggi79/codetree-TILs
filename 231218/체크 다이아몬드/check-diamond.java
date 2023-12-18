baf8d6f63c3a7a1c47b0ebe5c4190f263b148dc6
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2023-12-18 23:57:50 +0900

    Title: 체크 다이아몬드, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231218/체크 다이아몬드/check-diamond.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+
+        // 변수 선언 및 입력
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+
+        for(int i = 0; i < n; i++){
+
+            for(int j = 0; j<n-1-i; j++){
+                System.out.print(" ");
+            }
+
+            for(int k = 0; k < i+1; k++){
+
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+        for(int i = 0; i < n-1; i++){
+
+            for(int j = 0; j<i+1; j++){
+                System.out.print(" ");
+            }
+
+            for(int k = 0; k < n-i-1; k++){
+                System.out.print("* ");
+            }
+
+            System.out.println();
+        }
+
+
+    }
+}
