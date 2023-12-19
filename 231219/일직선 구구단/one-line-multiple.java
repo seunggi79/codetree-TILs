31504ff12786103a57cc2494d9e223c1c7da7271
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2023-12-19 22:04:42 +0900

    Title: 일직선 구구단, Time: 149ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231219/일직선 구구단/one-line-multiple.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+
+        // 변수 선언 및 입력
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i =1; i <= n; i++){
+
+            for(int j = 1; j <= n; j++){
+                System.out.print(i+" * "+j+" = "+i*j);
+                System.out.println();
+
+            }
+
+
+            
+        }
+
+    }
+}
