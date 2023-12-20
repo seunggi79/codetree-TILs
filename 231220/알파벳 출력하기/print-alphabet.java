57fbe3136103a04b68a574cfd3551ea7b5eaf56e
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2023-12-20 13:36:57 +0900

    Title: 알파벳 출력하기, Time: 130ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231220/알파벳 출력하기/print-alphabet.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+
+        // 변수 선언 및 입력
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int num = 65;
+        for (int i =1; i <= n; i++){
+
+            for(int j = 1; j <=i ; j++){
+                System.out.print((char)num);
+                num++;
+
+            }
+            System.out.println();
+
+        }
+
+    }
+}
