c32fa2fb82aaf70f3166409b35d8a83b58035cdd
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2023-12-21 15:07:59 +0900

    Title: 3n + 1 수열 m번, Time: 169ms, Memory: 10MB, Status: Passed - CodeTree

--- 231221/3n + 1 수열 m번/3n-plus-1-sequence-m-times.java
+++ 231221/3n + 1 수열 m번/3n-plus-1-sequence-m-times.java
@@ -6,22 +6,22 @@ public class Main {
         // 변수 선언 및 입력
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        for(int i=0; i<n; i++){
-            int m =sc.nextInt();
-            int cnt = 0;
-            while(m!=1){
-                if(m%2==0){
-                    m /=2;
-                    cnt+=1;
-                }else{
-                    m*=3;
-                    m+=1;
+
+        for (int i = 0; i <n; i++){
+            int m = sc.nextInt();
+            int cnt =0;
+            for (int j = m; j > 1;){
+                if (j %2==0){
+                    j /= 2;
                     cnt+=1;
                 }
-
+                else {
+                    j*=3;
+                    j+=1;
+                    cnt +=1;
+                }
             }
             System.out.println(cnt);
-
         }
 
     }
