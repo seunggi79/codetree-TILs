237e562555c1940d4873bdcb36e5342938f693ed
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2023-12-22 12:51:52 +0900

    Title: 특정 위치의 숫자, Time: 149ms, Memory: 10MB, Status: Passed - CodeTree

--- 231222/특정 위치의 숫자/numbers-in-specific-location.java
+++ 231222/특정 위치의 숫자/numbers-in-specific-location.java
@@ -5,15 +5,15 @@ public class Main {
 
         // 변수 선언 및 입력
         Scanner sc = new Scanner(System.in);
-        
+
         int[] arr = new int[10];
         int sum = 0;
         for(int i=0; i<10;i++){
             arr[i] = sc.nextInt();
-            sum += arr[i];
             
+
         }
-        System.out.println(sum);
+        System.out.println(arr[2]+arr[4]+arr[9]);
 
 
 
