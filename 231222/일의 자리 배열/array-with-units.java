d1276328691d825135b3fbec57df4aa004b8bb6a
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2023-12-22 14:33:04 +0900

    Title: 일의 자리 배열, Time: 133ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231222/일의 자리 배열/array-with-units.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+
+        // 변수 선언 및 입력
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] arr = new int[10];
+        arr[0]=n;
+        arr[1]=m;
+        for(int i =2; i<10;i++){
+            arr[i] = arr[i-2]+arr[i-1];
+            if(arr[i] >=10){
+                arr[i] = arr[i] % 10;
+            }
+        }
+
+        for(int i =0;i<10; i++){
+            System.out.println(arr[i]+" ");
+        }
+    }
+}
