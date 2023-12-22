b90371a3ffafea06edb06aafc4d9bd150a78c9be
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2023-12-22 12:51:01 +0900

    Title: 특정 위치의 숫자, Time: 122ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231222/특정 위치의 숫자/numbers-in-specific-location.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+
+        // 변수 선언 및 입력
+        Scanner sc = new Scanner(System.in);
+        
+        int[] arr = new int[10];
+        int sum = 0;
+        for(int i=0; i<10;i++){
+            arr[i] = sc.nextInt();
+            sum += arr[i];
+            
+        }
+        System.out.println(sum);
+
+
+
+    }
+}
