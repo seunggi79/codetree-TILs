0341f208ad89c700adb2acdba43b4a91daae3185
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2023-12-22 14:48:42 +0900

    Title: 숫자들의 배수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 231222/숫자들의 배수/multiple-of-numbers.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+
+        // 변수 선언 및 입력
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int cnt = 0;
+        int[] arr = new int[n];
+        for(int i =0; i<10;i++){
+            arr[i] = n*(i+1);
+            if(arr[i] % 5 == 0){
+                cnt++;
+            }
+            if(cnt == 2){
+                break;
+            }
+        }
+
+        for(int i =0; i<10; i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
