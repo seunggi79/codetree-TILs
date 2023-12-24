a7287ffc5fb0898f843738fbd30abc177988c283
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2023-12-24 20:23:02 +0900

    Title: 배열 놀이, Time: 166ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231224/배열 놀이/play-with-array.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+
+        // 변수 선언 및 입력
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int q = sc.nextInt();
+        int[] arr = new int[n];
+
+        for(int i = 0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+        for (int i =0; i<q; i++){
+            int m = sc.nextInt();
+            if(m == 1){
+                int k =sc.nextInt();
+                System.out.println(arr[k-1]);
+            } else if (m == 2) {
+                int k = sc.nextInt();
+                boolean swich = true;
+                for(int j = 0; j<n; j++){
+                   if (arr[j] == k){
+                       swich = false;
+                       System.out.println(j+1);
+                       break;
+                    }
+                }
+                if(swich){
+                    System.out.println(0);
+                }
+
+            } else if (m == 3) {
+                int k = sc.nextInt();
+                int l = sc.nextInt();
+                for(int j = k; j<=l; j++){
+                    System.out.print(arr[j-1]+" ");
+                }
+            }
+        }
+
+
+    }
+}
