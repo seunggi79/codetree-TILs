1f6d9d7ccfe3f448467f8c3fed0464db5f05b607
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2023-12-28 15:16:51 +0900

    Title: 500 근처의 수, Time: 153ms, Memory: 10MB, Status: Passed - CodeTree

--- 231228/500 근처의 수/near-500.java
+++ 231228/500 근처의 수/near-500.java
@@ -11,7 +11,7 @@ public class Main {
         for (int i = 0; i < 10; i++) {
             arr[i] = sc.nextInt();
         }
-        int max = arr[0];
+        int max = 1;
         int min = 1000;
         for(int i=0; i<10; i++){
             if(arr[i] < 500 && arr[i]>=max){
