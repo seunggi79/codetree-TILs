419a80b5e109315bbff14a32b54436d14b50ce25
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2023-12-30 00:08:47 +0900

    Title: 두 배열의 곱, Time: 123ms, Memory: 10MB, Status: Passed - CodeTree

--- 231230/두 배열의 곱/multiple-of-two-arrays.java
+++ 231230/두 배열의 곱/multiple-of-two-arrays.java
@@ -22,7 +22,7 @@ public class Main {
         }
         for(int i =0; i<3; i++) {
             for (int j = 0; j < 3; j++) {
-                System.out.print(arr[i][j]+arr2[i][j]+" ");
+                System.out.print(arr[i][j]*arr2[i][j]+" ");
             }
             System.out.println();
         }
