b2afdb68d5f842d441bae39fc7b34eb69cf3dacb
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2023-12-30 00:08:30 +0900

    Title: 두 배열의 곱, Time: 151ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 231230/두 배열의 곱/multiple-of-two-arrays.java
+++ 231230/두 배열의 곱/multiple-of-two-arrays.java
@@ -14,7 +14,6 @@ public class Main {
                 
             }
         }
-        System.out.println();
         for(int i =0; i<3; i++) {
             for (int j = 0; j < 3; j++) {
                 
