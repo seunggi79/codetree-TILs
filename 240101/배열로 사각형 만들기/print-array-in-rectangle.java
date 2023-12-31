45a1dbc72a252dcde0a5e801b1e78792968c7703
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2024-01-01 00:57:30 +0900

    Title: 배열로 사각형 만들기, Time: 119ms, Memory: 9MB, Status: Passed - CodeTree

--- 240101/배열로 사각형 만들기/print-array-in-rectangle.java
+++ 240101/배열로 사각형 만들기/print-array-in-rectangle.java
@@ -19,7 +19,7 @@ public class Main {
             }
         }
 
-        for(int i=0; i<n; i++){
+        for(int i=0; i<5; i++){
             for(int j =0; j<5; j++){
                 System.out.print(arr[i][j] + " ");
             }
