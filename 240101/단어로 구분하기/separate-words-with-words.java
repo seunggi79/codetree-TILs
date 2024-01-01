5f928c3d11fe882cc053a82add58f15862e177cc
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2024-01-01 14:48:40 +0900

    Title: 단어로 구분하기, Time: 121ms, Memory: 10MB, Status: Passed - CodeTree

--- 240101/단어로 구분하기/separate-words-with-words.java
+++ 240101/단어로 구분하기/separate-words-with-words.java
@@ -14,7 +14,7 @@ public class Main {
             
         }
         
-        for(int i = 0; i< n; i++){
+        for(int i = 0; i< 10; i++){
             System.out.println(str[i]);
         }
 
