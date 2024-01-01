6b87d7d4de91a0925ad566cf438e196a74733711
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2024-01-01 15:15:04 +0900

    Title: 특정 문자로 시작하는 문자열, Time: 142ms, Memory: 10MB, Status: Passed - CodeTree

--- 240101/특정 문자로 시작하는 문자열/strings-that-start-with-a-specific-character.java
+++ 240101/특정 문자로 시작하는 문자열/strings-that-start-with-a-specific-character.java
@@ -5,24 +5,24 @@ public class Main {
 
         // 변수 선언 및 입력
         Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String[] str = new String[n];
         int cnt = 0;
         int cnt2 = 0;
-        int n = sc.nextInt();
-
-        String[] str = new String[20];
         for(int i =0; i<n; i++){
             str[i] = sc.next();
-
         }
-        char s = sc.next().charAt(0);
-        for(int i =0; i<n; i++){
-            if(str[i].charAt(0) == s){
-                cnt++;
+        char c = sc.next().charAt(0);
+        
+        for(int i=0; i<n;i++){
+            if (str[i].charAt(0) == c){
+                cnt+=1;
                 cnt2 += str[i].length();
             }
-
         }
-        System.out.printf(cnt + " %.2f" ,(double)(cnt2/cnt));
+
+        System.out.printf(cnt+" %.2f", (double)cnt2/cnt);
+
 
     }
 }
