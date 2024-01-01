7a3d250190ac76ea3f4cd08b45fe46e68374012c
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2024-01-01 14:50:56 +0900

    Title: 단어로 구분하기 3, Time: 152ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240101/단어로 구분하기 3/separate-words-with-words-3.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+
+        // 변수 선언 및 입력
+        Scanner sc = new Scanner(System.in);
+        int cnt = 0;
+        int cnt2 = 0;
+
+        String[] str = new String[10];
+        for(int i =0; i<10; i++){
+            str[i] = sc.next();
+
+        }
+
+        for(int i = 9; i>=0; i--){
+            System.out.println(str[i]);
+        }
+
+
+
+    }
+}
