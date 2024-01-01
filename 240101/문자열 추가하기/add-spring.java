f564597bc80cb6414af60567495542c187a474c6
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2024-01-01 18:52:25 +0900

    Title: 문자열 추가하기, Time: 123ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240101/문자열 추가하기/add-spring.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+
+        // 변수 선언 및 입력
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        System.out.println(str+"Hello");
+
+    }
+}
