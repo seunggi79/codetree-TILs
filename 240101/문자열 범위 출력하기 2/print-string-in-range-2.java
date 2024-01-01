c784fed08e9b96dac72e872d0687a2025f3521b8
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2024-01-01 18:04:01 +0900

    Title: 문자열 범위 출력하기 2, Time: 146ms, Memory: 10MB, Status: Passed - CodeTree

--- 240101/문자열 범위 출력하기 2/print-string-in-range-2.java
+++ 240101/문자열 범위 출력하기 2/print-string-in-range-2.java
@@ -14,7 +14,7 @@ public class Main {
             }
         }
         else{
-            for(int i=str.length()-1; i>=0; i--){
+            for(int i=str.length()-1; i>=str.length()-n; i--){
                 System.out.print(str.charAt(i));
             }
         }
