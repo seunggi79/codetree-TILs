772bce4e8f5c7b742d950022b84dabe4850f8d25
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2024-01-01 14:40:20 +0900

    Title: 문자열 길이의 합, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240101/문자열 길이의 합/sum-length-of-string.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+
+        // 변수 선언 및 입력
+        Scanner sc = new Scanner(System.in);
+        int cnt = 0;
+        int cnt2 = 0;
+        int n = sc.nextInt();
+        String[] str = new String[n];
+        for(int i =0; i<n; i++){
+            str[i] = sc.next();
+            cnt += str[i].length();
+            for(int j =0; j<str.length; j++){
+                if (str[i].charAt(j) == 'a')
+                    cnt2 +=1;
+            }
+        }
+        System.out.println(cnt+ " "+cnt2 );
+
+
+
+    }
+}
