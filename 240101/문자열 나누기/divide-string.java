4eaf53a68c3aad1acde09f96b39c28c5cb06d2f0
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2024-01-01 18:58:01 +0900

    Title: 문자열 나누기, Time: 136ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240101/문자열 나누기/divide-string.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+
+        // 변수 선언 및 입력
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String str2 = "";
+        for(int i=0; i<n; i++){
+            String str = sc.next();
+            str2 += str;
+
+
+        }
+        for(int i = 0; i<str2.length(); i++){
+            for(int j =0; j<5; j++){
+                System.out.println(str2.charAt(i));
+            }
+            System.out.println();
+        }
+
+    }
+}
