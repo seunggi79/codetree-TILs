5ef91412787f01975eaca9568252284764ab6c84
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2024-01-01 23:36:43 +0900

    Title: 부분 문자열의 개수, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

--- 240101/부분 문자열의 개수/number-of-substrings.java
+++ 240101/부분 문자열의 개수/number-of-substrings.java
@@ -5,25 +5,15 @@ public class Main {
 
         // 변수 선언 및 입력
         Scanner sc = new Scanner(System.in);
-
-        String iStr = sc.next();
-        String tStr = sc.next();
-        
+        String str1 = sc.next();
+        String str2 = sc.next();
         int cnt = 0;
-        for(int i = 0; i<iStr.length(); i++){
-            boolean k =true;
-            
-            for(int j =0; j<tStr.length(); i++){
-                if(iStr.charAt(i+j) == tStr.charAt(j)){
-                    continue;
+        for(int i=0; i< str1.length()-1; i++){
+            for(int j =0; j<1; j++){
+                if(str1.charAt(i) == str2.charAt(j)&&str1.charAt(i+1) == str2.charAt(j+1)){
+                    cnt+=1;
                 }
-                k = false;
             }
-            
-            if(k){
-                cnt+=1;
-            }
-            
         }
         System.out.println(cnt);
     }
