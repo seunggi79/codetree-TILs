c731273596c7ad1fb6679a4815b482c162b99187
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2024-01-02 10:16:02 +0900

    Title: 첫 번째와 두 번째 교환, Time: 123ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240102/첫 번째와 두 번째 교환/exchange-1st-and-2nd.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+
+        // 변수 선언 및 입력
+        Scanner sc = new Scanner(System.in);
+        String str1 = sc.next();
+        char c1 = str1.charAt(0);
+        char c2 = str1.charAt(1);
+        char[] arr = str1.toCharArray();
+        for(int i =0; i<str1.length(); i++){
+            if (arr[i] == c1){
+                arr[i] = c2;
+            }
+            else if (arr[i] == c2){
+                arr[i] = c1;
+            }
+        }
+        System.out.println(String.valueOf(arr));
+
+    }
+}
