51ec89afe9479c45e172265600b7d79256d2439c
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2024-01-02 14:35:01 +0900

    Title: 문자열 밀기, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

--- 240102/문자열 밀기/push-char.java
+++ 240102/문자열 밀기/push-char.java
@@ -5,8 +5,8 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
         int len = str.length();
-        str = str.substring(1, len-1)+str.substring(0,1);
+        str = str.substring(1, len)+str.substring(0,1);
         System.out.println(str);
-
+        
     }
 }
