5555739d25e1483bee25cf41a337831f5f3764f0
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2024-01-02 10:32:42 +0900

    Title: 문자열 놀이, Time: 162ms, Memory: 10MB, Status: Passed - CodeTree

--- 240102/문자열 놀이/play-with-string.java
+++ 240102/문자열 놀이/play-with-string.java
@@ -9,7 +9,7 @@ public class Main {
         int n = sc.nextInt();
         char[] arr = str1.toCharArray();
 
-        for(int i=0; i<2; i++){
+        for(int i=0; i<n; i++){
             int k = sc.nextInt();
             if(k == 1){
                 int l1 = sc.nextInt();
