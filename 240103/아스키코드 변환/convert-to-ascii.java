4d6664afa61b32612ef12adeb260e66becdf6cce
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2024-01-03 20:58:55 +0900

    Title: 아스키코드 변환, Time: 149ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240103/아스키코드 변환/convert-to-ascii.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        char c = sc.next().charAt(0);
+        int n = sc.nextInt();
+
+
+        System.out.print((int)c+" "+(char)n);
+
+    }
+}
