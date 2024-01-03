4130881e6315406d53ebf66981ccddc14859c7e0
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2024-01-04 00:45:19 +0900

    Title: 각 자리 숫자들의 합, Time: 140ms, Memory: 10MB, Status: Passed - CodeTree

--- 240104/각 자리 숫자들의 합/sum-of-each-digit.java
+++ 240104/각 자리 숫자들의 합/sum-of-each-digit.java
@@ -4,11 +4,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n =sc.nextInt();
-        
+
         int cnt =0;
         String str = Integer.toString(n);
+        char[] arr = str.toCharArray();
         for(int i=0; i< str.length(); i++){
-            cnt+=str.charAt(i);
+            cnt+=(int)arr[i]-'0';
+            
+            
         }
         System.out.println(cnt);
 
