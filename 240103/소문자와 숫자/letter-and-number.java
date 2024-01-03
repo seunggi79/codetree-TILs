61aebea6a1a16139ea31ba253a83c109aadaf4db
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2024-01-03 22:08:04 +0900

    Title: 소문자와 숫자, Time: 141ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240103/소문자와 숫자/letter-and-number.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+        char[] arr = str.toCharArray();
+
+        for(int i=0; i<str.length(); i++){
+            if('a'<=arr[i] && arr[i] <= 'z'){
+                System.out.print(arr[i]);
+            } else if ('A'<=arr[i] && arr[i] <= 'Z') {
+                System.out.print((char)(arr[i]+32));
+            }
+        }
+
+    }
+}
