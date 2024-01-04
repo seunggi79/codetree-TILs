8449fb5b75068df48aecda0bf2c921e212bba362
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2024-01-04 12:03:58 +0900

    Title: 미는 횟수, Time: 131ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240104/미는 횟수/number-of-pushes.java
+++ 240104/미는 횟수/number-of-pushes.java
@@ -6,28 +6,23 @@ public class Main {
 
         String str = sc.next();
         String str2 = sc.next();
-        if(str.length() != str2.length()){
-            System.out.println(-1);
-            System.exit(0);
-        }
-
-        String a = str;
-        String[] b = new String[str.length()];
-        int cnt =0;
-        while(true){
-            b[cnt] = str;
-            str = str.substring(str.length()-1,str.length())+str.substring(0,str.length()-1);
+        int cnt = 0;
+        int cnt2 = 0;
+        // 문자열의 길이 만큼 반복
+        for(int i =0; i<str.length(); i++){
+            str = str.substring(str.length()-1, str.length())+str.substring(0,str.length()-1);
             cnt++;
-
-            if(str.equals(a)){
+            if(str.equals(str2)){
                 break;
             }
-        }
-        for(int i=0;i<cnt; i++){
-            if(b[i].equals(str2)){
-                System.out.println(i);
+            else{
+                cnt2++;
             }
         }
-
+        if(cnt2 == 6){
+            System.out.println(-1);
+        }
+        else
+            System.out.println(cnt);
     }
 }
