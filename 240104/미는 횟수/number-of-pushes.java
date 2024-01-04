e20ca61b907fad27feaf080ed968fc419a68276a
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2024-01-04 11:50:49 +0900

    Title: 미는 횟수, Time: 831ms, Memory: 176MB, Status: Memory Limit Exceed - CodeTree

---
+++ 240104/미는 횟수/number-of-pushes.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+        String str2 = sc.next();
+        int cnt =0;
+        while(true){
+            str = str.substring(str.length()-1,str.length())+str.substring(0,str.length()-1);
+            cnt++;
+            if(str.compareTo(str2) == 0){
+                break;
+            }
+        }
+        System.out.println(cnt);
+
+    }
+}
