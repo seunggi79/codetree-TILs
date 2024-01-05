d9a14e150a6b32c323d2d3911e1534e5ceb43090
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2024-01-05 11:39:12 +0900

    Title: 100 도달하기, Time: 158ms, Memory: 10MB, Status: Passed - CodeTree

--- 240105/100 도달하기/reach-100.java
+++ 240105/100 도달하기/reach-100.java
@@ -7,7 +7,8 @@ public class Main {
         int[] arr = new int[100];
         arr[0] = 1;
         arr[1] = n;
-
+        System.out.print(arr[0]+" ");
+        System.out.print(arr[1]+" ");
         for(int i =2; i<=100; i++){
             arr[i] = arr[i-2]+arr[i-1];
             System.out.print(arr[i]+" ");
