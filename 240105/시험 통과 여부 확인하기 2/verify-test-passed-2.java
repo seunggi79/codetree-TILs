8f9cb85f4720ac5a03ff21503bc29053ba2b8095
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2024-01-05 11:27:26 +0900

    Title: 시험 통과 여부 확인하기 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240105/시험 통과 여부 확인하기 2/verify-test-passed-2.java
+++ 240105/시험 통과 여부 확인하기 2/verify-test-passed-2.java
@@ -5,11 +5,15 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] arr = new int[4];
-        int sum = 0;
+
         int cnt = 0;
         for(int i=0; i<n; i++){
-            arr[i] = sc.nextInt();
-            sum += arr[i];
+            int sum = 0;
+            for(int j=0; j<n; j++){
+                arr[j] = sc.nextInt();
+                sum += arr[j];
+
+            }
             if(sum/4 >= 60){
                 System.out.println("pass");
                 cnt++;
@@ -17,7 +21,9 @@ public class Main {
                 System.out.println("fail");
             }
 
+
         }
+
 
         System.out.println(cnt);
     }
