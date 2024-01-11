9ad334d1fef71f7b3568df53e1d26958f7513606
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2024-01-11 19:13:23 +0900

    Title: 함수를 이용한 온전수 판별, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

--- 240111/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.java
+++ 240111/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.java
@@ -7,16 +7,17 @@ public class Main {
         int m = sc.nextInt();
 
         System.out.println(print(n, m));
-
-
-
     }
     public static int print(int a, int b) {
         int cnt = 0;
         for(int i=a; i<=b; i++){
 
-            if(i %2 != 0 && i%10 != 5 && i % 3 != 0){
-                cnt++;
+            if(i % 2 != 0){
+                if(i%10 != 5){
+                    if(i % 3 != 0 || i % 9 ==0){
+                        cnt++;
+                    }
+                }
             }
 
         }
