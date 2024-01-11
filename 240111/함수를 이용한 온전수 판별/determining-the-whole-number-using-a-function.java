c9510ad8aafdbc0645f2e4caf6b457ce4b678298
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2024-01-11 18:52:16 +0900

    Title: 함수를 이용한 온전수 판별, Time: 126ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240111/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        System.out.println(print(n, m));
+
+
+
+    }
+    public static int print(int a, int b) {
+        int cnt = 0;
+        for(int i=a; i<=b; i++){
+
+            if(i %2 != 0 && i%10 != 5 && i % 9 != 0){
+                cnt++;
+            }
+
+        }
+        return cnt;
+    }
+
+}
