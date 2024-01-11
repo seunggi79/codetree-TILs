294d99812002ba73e6f3869659918e38ddb01655
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2024-01-11 19:11:14 +0900

    Title: 함수를 이용한 온전수 판별, Time: 126ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240111/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.java
+++ 240111/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.java
@@ -15,7 +15,7 @@ public class Main {
         int cnt = 0;
         for(int i=a; i<=b; i++){
 
-            if(i %2 != 0 && i%10 != 5 && i % 9 != 0){
+            if(i %2 != 0 && i%10 != 5 && i % 3 != 0){
                 cnt++;
             }
 
