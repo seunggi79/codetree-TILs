29ed96d11404f90fcdf38bd688b3416ece09a6a1
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2024-01-12 10:30:44 +0900

    Title: 2021년 날짜의 유무, Time: 133ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240112/2021년 날짜의 유무/with-or-without-2021.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+        System.out.println(print(n1, n2));
+    }
+
+    public static String print(int a, int b){
+
+        if(a == 2){
+            if(b<=28){
+                return "Yes";
+            }
+            else{
+                return "No";
+            }
+        }
+        else if(a < 8 && a % 2 != 0){
+            if(b<=31){
+                return "Yes";
+            }else{
+                return "No";
+            }
+
+        }
+        else if (a >= 8 && a % 2 ==0){
+            if (b<=31){
+                return "Yes";
+            }
+            else{
+                return "No";
+            }
+
+        }
+        else{
+            return "No";
+        }
+    }
+
+}
