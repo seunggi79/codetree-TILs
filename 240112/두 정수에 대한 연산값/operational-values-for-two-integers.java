449a07fd133c78b28400bca276c9dc8c390c8bf4
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2024-01-12 17:00:44 +0900

    Title: 두 정수에 대한 연산값, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240112/두 정수에 대한 연산값/operational-values-for-two-integers.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+class IntWrapper {
+    int value;
+
+    public IntWrapper(int value) {
+        this.value = value;
+    }
+}
+
+
+
+public class Main {
+
+
+    public static void print(IntWrapper a, IntWrapper b){
+        if(a.value>b.value){
+            a.value += 25;
+            b.value *= 2;
+        }
+        else{
+            b.value += 25;
+            a.value *= 2;
+        }
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        IntWrapper a = new IntWrapper(n);
+        IntWrapper b = new IntWrapper(m);
+        print(a, b);
+        n = a.value;
+        m = b.value;
+        System.out.println(n+" "+m);
+    }
+
+}
