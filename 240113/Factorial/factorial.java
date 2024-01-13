ac3aadda8952760bd3e7ed80a2a0297edf3faf39
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2024-01-13 21:40:12 +0900

    Title: Factorial, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240113/Factorial/factorial.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static int print(int n) {
+        if(n==0){
+            return 1;
+        }
+        return print(n-1) * n;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+
+        System.out.println(print(n));
+
+
+    }
+
+}
