45d0599010561e86684eeac868130b6e2f209a16
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2024-01-15 12:08:43 +0900

    Title: 007, Time: 135ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240115/007/007.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+class Print{
+    String str;
+    char c;
+    int n;
+    public Print(String str, char c, int n){
+        this.str = str;
+        this.c = c;
+        this.n = n;
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.next();
+        char c = sc.next().charAt(0);
+        int n = sc.nextInt();
+
+        Print print = new Print(str, c, n);
+
+        System.out.println("secret code : "+str);
+        System.out.println("meeting point : "+c);
+        System.out.println("time : "+n);
+
+
+
+
+
+    }
+
+}
