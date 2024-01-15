bcb1bdaefa7142fb2533fd1ff842936c4da0b846
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2024-01-15 13:23:18 +0900

    Title: 상품 코드, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

--- 240115/상품 코드/product-code.java
+++ 240115/상품 코드/product-code.java
@@ -10,7 +10,7 @@ class Print {
     public Print(String str, int n){
 
         this.str = str;
-        
+
         this.n = n;
     }
 }
@@ -19,11 +19,11 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
         int n = sc.nextInt();
-        
+
         Print print1 = new Print("codetree", 50);
         Print print2 = new Print(str, n);
-        System.out.println("product : " + print1.n+" is "+print1.str);
-        System.out.println("product : " + print2.n+" is "+print2.str);
+        System.out.println("product " + print1.n+" is "+print1.str);
+        System.out.println("product " + print2.n+" is "+print2.str);
 
     }
 
