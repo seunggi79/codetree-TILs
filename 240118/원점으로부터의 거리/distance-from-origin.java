8e0ad80334b057ee2aa453d6f40ed6fffc38fc1b
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2024-01-18 01:33:24 +0900

    Title: 원점으로부터의 거리, Time: 129ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240118/원점으로부터의 거리/distance-from-origin.java
@@ -0,0 +1,56 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Scanner;
+
+class Print{
+
+
+    int x;
+    int y;
+    int num;
+
+    public Print( int x, int y, int num){
+
+        this.x = x;
+        this.y = y;
+        this.num = num;
+
+    }
+
+}
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        Print[] print = new Print[n];
+        for(int i =0; i<n; i++){
+
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+
+            print[i] = new Print(x, y, i+1);
+
+        }
+
+        Arrays.sort(print, new Comparator<Print>() {
+            @Override
+            public int compare(Print o1, Print o2) {
+                return ((o1.x * o1.x)+(o1.y * o1.y)) - ((o2.x * o2.x)+(o2.y * o2.y));
+            }
+        });
+
+
+        for(int i=0; i<n; i++){
+            System.out.println(print[i].num);
+        }
+
+
+
+
+
+
+    }
+
+}
