cd484501ed46d27672d7befa001950ab28fdb9fb
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2024-01-18 10:57:10 +0900

    Title: DateTime to DateTime, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

--- 240118/DateTime to DateTime/datetime-to-datetime.java
+++ 240118/DateTime to DateTime/datetime-to-datetime.java
@@ -17,11 +17,11 @@ public class Main {
             System.out.println(-1);
             System.exit(0);
         }
-        if(11 > b){
+        if(11 > b && 11==a){
             System.out.println(-1);
             System.exit(0);
         }
-        if(11 > c){
+        if(11 > c&&a==11&&b==11){
             System.out.println(-1);
             System.exit(0);
         }
