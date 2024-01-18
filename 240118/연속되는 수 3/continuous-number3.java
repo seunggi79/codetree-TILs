23516e699a16fb1feb8cd31b150411b9973cec59
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2024-01-18 21:09:52 +0900

    Title: 연속되는 수 3, Time: 194ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 240118/연속되는 수 3/continuous-number3.java
@@ -0,0 +1,32 @@
+import java.awt.print.Pageable;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        int ans =0, cnt = 0;
+        for(int i=0; i<n; i++){
+            // 부호가 같으면 +1
+            if(i >= 1 && arr[i] * arr[i - 1] > 0)
+                cnt++;
+            else // 부호가 다른 경우
+                cnt = 1;
+
+            ans = Math.max(ans, cnt);
+        }
+        System.out.println(ans);
+    }
+
+
+}
