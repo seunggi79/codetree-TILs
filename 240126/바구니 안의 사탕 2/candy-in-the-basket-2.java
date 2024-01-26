8ed26fa1485f15f7b1a260a4811fdc9dd7f0c7e5
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2024-01-26 14:53:49 +0900

    Title: 바구니 안의 사탕 2, Time: 151ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240126/바구니 안의 사탕 2/candy-in-the-basket-2.java
+++ 240126/바구니 안의 사탕 2/candy-in-the-basket-2.java
@@ -21,7 +21,7 @@ public class Main {
         }
 
         int ans = 0;
-        for(int i=0; i<100; i++){
+        for(int i=0; i<=100; i++){
             int sum = 0;
             for(int j = i-m; j<=i+m; j++){
                 if(j<0){
