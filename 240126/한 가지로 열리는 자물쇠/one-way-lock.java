7e603f5c84778f887211f0366313ba11df325987
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2024-01-26 19:45:41 +0900

    Title: 한 가지로 열리는 자물쇠, Time: 118ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240126/한 가지로 열리는 자물쇠/one-way-lock.java
@@ -0,0 +1,31 @@
+import java.awt.print.Pageable;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        // 각 자리 자물쇠의 차이가 2이하면 자물쇠는 열림
+        int cnt = 0;
+        for(int i =a+2; i<6; i++){
+
+            for(int j=b+2; j<6; j++){
+
+                for(int k=c+2; k<6; k++){
+                    cnt++;
+
+                }
+            }
+        }
+        System.out.println(n*n*n - cnt);
+    }
+}
