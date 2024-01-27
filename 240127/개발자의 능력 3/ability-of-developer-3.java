27a1bd60bfca122274c60ba6c26f5dbab715e124
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2024-01-27 20:38:40 +0900

    Title: 개발자의 능력 3, Time: 125ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240127/개발자의 능력 3/ability-of-developer-3.java
+++ 240127/개발자의 능력 3/ability-of-developer-3.java
@@ -6,13 +6,14 @@ import java.util.Scanner;
 
 public class Main {
     public static int[] arr = new int[6];
+    public static int INT_MIN = Integer.MIN_VALUE;
     public static int getDiff(int i, int j, int k){
         int sum1 = arr[i] +arr[j] +arr[k];
         int sum2 = 0;
 
         for(int l =0; l<6; l++){
             sum2 += arr[l];
-        }   
+        }
         sum2 -= sum1;
 
 
@@ -29,7 +30,7 @@ public class Main {
             arr[i] = sc.nextInt();
         }
 
-        int min = 100;
+        int min = INT_MIN;
         for(int i=0; i<6; i++){
             for(int j =i+1; j<6; j++){
                 for(int k=j+1; k<6; k++){
