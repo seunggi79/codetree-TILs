4c13f523d89f6f958a8af56949724dbc4977e594
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2024-02-06 12:18:14 +0900

    Title: 등차수열, Time: 143ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240206/등차수열/arithmetic-sequence.java
@@ -0,0 +1,34 @@
+import java.awt.print.Pageable;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class Main {
+
+    public static int INT_MAX = Integer.MAX_VALUE;
+    public static int INT_MIN = Integer.MIN_VALUE;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+
+        for(int i =0; i<n; i++){
+            a[i] = sc.nextInt();
+        }
+
+        int cnt =0;
+        for(int i=0; i<n; i++){
+            for(int k = 1; k<= 100; k++){
+                for(int j =i+1; j<n; j++){
+                    if(a[j] - k == k - a[i]){
+                        cnt++;
+                    }
+                }
+            }
+        }
+        System.out.println(cnt);
+
+    }
+}
