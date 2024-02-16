28f157712896860117442cde50ed3f6d21638f9e
seunggi79 <130640157+seunggi79@users.noreply.github.com>
2024-02-16 18:29:04 +0900

    Title: 최대값을 출력하는 함수, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240216/최대값을 출력하는 함수/function-that-outputs-maximum-value.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+        int n3 = sc.nextInt();
+
+        int max = Math.max(n1, n2);
+        max = Math.max(max, n3);
+        System.out.println(max);
+    }
+}
